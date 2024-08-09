785946baf1258f46df3bf4fdbd484747ea9c5132
nabilBouzineDev <nabilsocialnetwork@gmail.com>
2024-08-09 13:00:39 +0100

    set up Project

---
+++ app/src/main/java/com/nabilbdev/searchflight/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.nabilbdev.searchflight
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.nabilbdev.searchflight.ui.theme.SearchFlightTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SearchFlightTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.surface
+                ) {
+                    // Code Here...
+                }
+            }
+        }
+    }
+}
