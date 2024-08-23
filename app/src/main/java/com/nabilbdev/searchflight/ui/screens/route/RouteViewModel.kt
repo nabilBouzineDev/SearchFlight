e7487fd063299026d2fd734663cc689ece31419b
nabilBouzineDev <nabilsocialnetwork@gmail.com>
2024-08-23 10:03:49 +0100

    added route screen UI

---
+++ app/src/main/java/com/nabilbdev/searchflight/ui/screens/route/RouteViewModel.kt
@@ -0,0 +1,10 @@
+package com.nabilbdev.searchflight.ui.screens.route
+
+import androidx.lifecycle.ViewModel
+import com.nabilbdev.searchflight.data.local.repository.SearchFlightRepository
+
+class RouteViewModel(
+    private val searchFlightRepository: SearchFlightRepository
+) : ViewModel() {
+
+}
